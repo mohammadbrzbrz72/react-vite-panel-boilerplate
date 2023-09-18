5e706a5a8ab58a2161f171b425b7075fc9105caf
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2023-09-18 20:22:39 +0330

    add user routes

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,5 +1,6 @@
 // handler.js is a module where we will have all our request handlers.
 // The handlers from this module can be reused for both browser and Node.
 import authRoutes from "./routes/auth";
+import userRoutes from "./routes/user";
 
-export const handlers = [authRoutes].flat();
+export const handlers = [authRoutes, userRoutes].flat();
