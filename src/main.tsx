cead9dba6758384bb410a34e55d36104d88dbf2e
mohammadbrzbrz72 <mohammadbrzbrz72@gmail.com>
2023-09-18 18:13:58 +0330

    set in main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-
+import App from "./App";
 // global styles
-import './styles/index.css';
-import './styles/globals.css';
+import "./styles/globals.css";
+import "./styles/index.css";
 
-const root = document.getElementById('root') as HTMLElement;
+// Start Mock Service Worker in development mode
+if (import.meta.env.DEV) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
+const root = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(root).render(<App />);
